6cdc10768c87ab322f7d3ee70ece4ca0da0ff5c2
Nadeem Sharief B <63586130+NadeemSB@users.noreply.github.com>
2024-06-18 14:24:28 +0530

    Adding generating count

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const names = ["Aman", "Arun", "Laxmi", "Anuj", "Kartick", "Ganesh", "Shashank", "Harika", "Palvee", "Chandra", "Manju", "Viraj", "Srashti", "Ritu", "Anshita", "Avanish", "Shruthi", "Nadeem"];
-
+let clickCount = 0;
 
 document.getElementById('pickNameButton').addEventListener('click', function() {
-
+    clickCount++;
     const randomIndex = Math.floor(Math.random() * names.length);
     const randomName = names[randomIndex];
 
     document.getElementById('result').textContent = `This week's Host: ${randomName}`;
+    document.getElementById('clickCount').textContent = `Generated: ${clickCount} times`;
 });
+
+
+// Initialize the checkboxes when the page loads
+document.addEventListener('DOMContentLoaded', createCheckboxes);
