a5d5e9a03d3904964eaa5bc34f8773ec6850180d
Nadeem Sharief B <63586130+NadeemSB@users.noreply.github.com>
2024-06-18 14:50:56 +0530

    Adding refresh button and team members

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const names = ["Aman", "Arun", "Laxmi", "Anuj", "Kartick", "Ganesh", "Shashank", "Harika", "Palvee", "Chandra", "Manju", "Viraj", "Srashti", "Ritu", "Anshita", "Avanish", "Shruthi", "Nadeem"];
 let clickCount = 0;
 
+function populateNameList() {
+    const nameList = document.getElementById('nameList');
+    nameList.innerHTML = '';
+
+    names.forEach(name => {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        nameList.appendChild(listItem);
+    });
+}
+
 document.getElementById('pickNameButton').addEventListener('click', function() {
     clickCount++;
     const randomIndex = Math.floor(Math.random() * names.length);
@@ -8,8 +19,13 @@ document.getElementById('pickNameButton').addEventListener('click', function() {
 
     document.getElementById('result').textContent = `This week's Host: ${randomName}`;
     document.getElementById('clickCount').textContent = `Generated: ${clickCount} times`;
+    document.getElementById('refreshButton').addEventListener('click', function() {
+        location.reload();
+    });
 });
 
 
 // Initialize the checkboxes when the page loads
-document.addEventListener('DOMContentLoaded', createCheckboxes);
+document.addEventListener('DOMContentLoaded', () => {
+    populateNameList();
+});
